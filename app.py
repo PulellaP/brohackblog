775a1d2f1d0cc20973cfa72294cd3fa9b1cbf174
PulellaP <ppulella@gmail.com>
2020-06-08 10:52:50 -0400

    did a bunch of stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 from datetime import datetime
 from flask_dance.contrib.github import make_github_blueprint, github
 from flask_login import UserMixin, current_user, LoginManager, login_required, login_user, logout_user
@@ -7,20 +8,20 @@ from flask_dance.consumer.storage.sqla import OAuthConsumerMixin, SQLAlchemyStor
 from flask_dance.consumer import oauth_authorized
 from sqlalchemy.orm.exc import NoResultFound
 from werkzeug.utils import secure_filename
+from markdown2 import Markdown
 import os
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nerdchat.db'
-app.config['SECRET_KEY'] = 'thisissupposedtobesecret'
-app.config['IMAGE_UPLOADS'] = os.getcwd()+'/static/images'
-app.config['ALLOWED_IMAGE_EXTENSIONS'] = ['PNG', 'JPG', 'JPEG', 'SVG']
-app.config['IMAGE_SIZE_LIMIT'] = 1603900
+app.config['SECRET_KEY'] = os.urandom(16)
+#app.config['IMAGE_UPLOADS'] = os.getcwd()+'/static/images'
+#app.config['ALLOWED_IMAGE_EXTENSIONS'] = ['PNG', 'JPG', 'JPEG', 'SVG']
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
-if os.path.isdir(app.config['IMAGE_UPLOADS']) == False:
-    os.mkdir(app.config['IMAGE_UPLOADS'])
+#if os.path.isdir(app.config['IMAGE_UPLOADS']) == False:
+#    os.mkdir(app.config['IMAGE_UPLOADS'])
 
 def allowed_image(filename):
     if not '.' in filename:
@@ -48,6 +49,7 @@ class Article(db.Model):
     title = db.Column(db.String(250), nullable=False)
     content = db.Column(db.Text, nullable=False)
     date_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    head_image = db.Column(db.String(500))
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -71,11 +73,10 @@ def github_login():
     if account_info_json['login'] == 'PulellaP' or 'RosarioPulella':
         account_user.is_author = True
         db.session.commit()
+        return redirect(url_for('index', page_num=1))
 
-    if account_user.is_author == True:
-        return f"<h1>Your github name is {account_info_json['login']} and you are an author</h1>"
     else:
-        return f"<h1>Your github name is {account_info_json['login']}</h1>"
+        return redirect(url_for('index', page_num=1))
 
 @oauth_authorized.connect_via(github_blueprint)
 def github_logged_in(blueprint, token):
@@ -104,7 +105,8 @@ def landing():
 @app.route('/home/<int:page_num>')
 def index(page_num):
     page_name = 'Articles'
-    post_page = Article.query.paginate(per_page=5, page=page_num, error_out=True)
+    #post_page = Article.query.paginate(per_page=5, page=page_num, error_out=True)
+    post_page = Article.query.order_by(Article.article_id.desc()).paginate(per_page=5, page=page_num, error_out=True)
     return render_template('index.html', post_page=post_page)
 
 @app.route('/logout')
@@ -122,22 +124,19 @@ def Write_Article():
         author = account_info_json['login']
         post_title = request.form['title']
         post_content = request.form['content']
-        if request.files:
-            file = request.files['myImage']
-            if file.filename == '':
-                return render_template('Write-Article.html', page_name = 'New Post', alert = 'Image must have a filename')
-            if allowed_image(file.filename) == False:
-                return render_template('Write-Article.html', page_name = 'New Post', alert = 'Invalid filename')
-
-            else:
-                if sys.getsizeof(file) > app.config['IMAGE_SIZE_LIMIT']:
-                    alertvar = 'Image size limit is ' + str(app.config['IMAGE_SIZE_LIMIT'])
-                    return render_template('Write-Article.html', page_name = 'New Post', alert = alertvar)
-                filename = secure_filename(file.filename)
-                directory = os.path.join(app.config['IMAGE_UPLOADS'], author)
-                if os.path.isdir(directory) == False:
-                    os.mkdir(directory)
-                file.save(os.path.join(directory, filename))
+        #if request.files:
+        #    file = request.files['myImage']
+        #    if file.filename == '':
+        #        return render_template('Write-Article.html', page_name = 'New Post', alert = 'Image must have a filename')
+        #    if allowed_image(file.filename) == False:
+        #        return render_template('Write-Article.html', page_name = 'New Post', alert = 'Invalid filename')
+        #
+        #    else:
+        #        filename = secure_filename(file.filename)
+        #        directory = os.path.join(app.config['IMAGE_UPLOADS'], author)
+        #        if os.path.isdir(directory) == False:
+        #            os.mkdir(directory)
+        #        file.save(os.path.join(directory, filename))
         new_post = Article(author = author, title = post_title, content = post_content)
         db.session.add(new_post)
         db.session.commit()
@@ -146,6 +145,20 @@ def Write_Article():
 
     else:
         return render_template('Write-Article.html', page_name = 'New Post')
+
+
+@app.route('/articles/<int:id>/edit', methods = ['GET', 'POST'])
+@login_required
+def Edit_Articles(id):
+    article = Article.query.filter_by(article_id=id).first()
+    if request.method == 'GET' and current_user.username == article.author:
+        return render_template('edit.html', content = article.content, title = article.title, author = article.author, id = id)
+
+    if request.method == 'POST' and current_user.username == article.author:
+        article.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('display_article', id=id))
+
 
 @app.route('/users/<string:users>')
 def show_users_page(users):
@@ -159,7 +172,8 @@ def show_users_page(users):
 @app.route('/articles/<int:id>')
 def display_article(id):
     article = Article.query.filter_by(article_id=id).first()
-    return render_template('article.html', title = article.title, content = article.content, author = article.author)
+    md = Markdown()
+    return render_template('article.html', title = article.title, content = md.convert(article.content), author = article.author)
 
 
 if __name__ == "__main__":
